e45466a7a0174d35b93756154df130133a1e0366
Thiago Pereira <thiagopereiramail@gmail.com>
2022-09-24 11:30:19 -0300

    Getting started with Ag Grid

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
